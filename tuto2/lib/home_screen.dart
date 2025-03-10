23655d6cb06aa657d734baee264ae6915bfd6271
WhyRoX <enes.12.yumer@gmail.com>
2025-03-11 00:06:32 +0100

    T02.5 Filtrage de liste

--- tuto2/lib/home_screen.dart
+++ tuto2/lib/home_screen.dart
@@ -5,27 +5,52 @@ import 'package:flutter/material.dart';
 import 'contact.dart';
 import 'contact_row.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var showFavorites = false;
+
+  void _toggleFavorites() {
+    setState(() {
+      showFavorites = !showFavorites;
+    });
+  }
+
+  @override
+  @override
   Widget build(BuildContext context) {
+    final displayedContacts = [
+      for (var contact in defaultContacts)
+        if (!showFavorites || contact.isFavorite) contact,
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Contact list"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            icon: Icon(showFavorites ? Icons.star : Icons.star_border),
+            onPressed: _toggleFavorites,
+          ),
+        ],
       ),
-      body: Padding(
+      body: Container(
         padding: const EdgeInsets.all(16.0),
         child: Center(
           child: SizedBox(
-            width: 512,
+            width: 512.0,
             child: ListView.separated(
-              itemCount: defaultContacts.length,
+              itemCount: displayedContacts.length,
               separatorBuilder: (context, index) => const Divider(),
               itemBuilder:
                   (context, index) =>
-                      ContactRow(contact: defaultContacts[index]),
+                      ContactRow(contact: displayedContacts[index]),
             ),
           ),
         ),
