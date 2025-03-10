0a0922981c437b82e67b4360e263dd66dec145ae
WhyRoX <enes.12.yumer@gmail.com>
2025-03-10 23:18:17 +0100

    TO2.1 Contacts

---
+++ tuto2/lib/contact.dart
@@ -0,0 +1,11 @@
+class Contact {
+  final String name;
+  final String phone;
+  final bool isFavorite;
+
+  Contact ({
+    required this.name,
+    required this.phone,
+    this.isFavorite = false,
+  });
+}
